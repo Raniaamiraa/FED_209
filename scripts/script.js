6e2ba7fb1e103dbf7ff86228251b6682650da5b0
Rania Belhimer <152303165+Raniaamiraa@users.noreply.github.com>
2024-09-12 23:14:00 +0200

    Begin html

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+//onderstaande code (nav) heb ik van chat gpt en heb ik aangepast naar mijn website 
+//document.getElementById('menu-btn').addEventListener('click', function() {
+ //   const navMenu = document.getElementById('nav-menu');
+  //  const isExpanded = this.getAttribute('aria-expanded') === 'true';
+
+ //   navMenu.classList.toggle('show');
+  //  this.setAttribute('aria-expanded', !isExpanded);
+//});
+
